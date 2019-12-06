abda49a1ee5df7db76f5a552e8fc36d2deb7d913
sanjay <sanjaytiwari037@gmail.com>
2019-12-06 20:38:37 +0530

    Added Json file & Integrate data into product table

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -14,14 +14,16 @@ function Home() {
         <p><a rel="Sashidhar Kundanamadugu" href="https://w3.ibm.com/bluepages/profile.html?uid=04644E744" target="_blank">Sashidhar Kundanamadugu(sashik12@in.ibm.com) Team Captain</a></p>
         <div className="teammembers">
           <table id="customers">
-            <tr>
-              <td><a rel="Srinivasulu Konkala" href="https://w3.ibm.com/bluepages/profile.html?uid=048565744" target="_blank">Srinivasulu Konkala(srinivasulu.konkala@in.ibm.com)</a></td>
-              <td><a rel="Supratim Maiti" href="https://w3.ibm.com/bluepages/profile.html?uid=926276744" target="_blank">Supratim Maiti(supratim.Maiti@in.ibm.com)</a></td>
-            </tr>
-            <tr>
-              <td><a rel="Dipak Dutta" href="https://w3.ibm.com/bluepages/profile.html?uid=04869Z744" target="_blank">Dipak Dutta(dirdutta@in.ibm.com)</a></td>
-              <td><a rel="Sanjay Tiwari" href="https://w3.ibm.com/bluepages/profile.html?uid=0591B4744" target="_blank">Sanjay Tiwari(sanjayti@in.ibm.com)</a></td>
-            </tr>
+            <tbody>
+                <tr>
+                    <td><a rel="Srinivasulu Konkala" href="https://w3.ibm.com/bluepages/profile.html?uid=048565744" target="_blank">Srinivasulu Konkala(srinivasulu.konkala@in.ibm.com)</a></td>
+                    <td><a rel="Supratim Maiti" href="https://w3.ibm.com/bluepages/profile.html?uid=926276744" target="_blank">Supratim Maiti(supratim.Maiti@in.ibm.com)</a></td>
+                </tr>
+                <tr>
+                    <td><a rel="Dipak Dutta" href="https://w3.ibm.com/bluepages/profile.html?uid=04869Z744" target="_blank">Dipak Dutta(dirdutta@in.ibm.com)</a></td>
+                    <td><a rel="Sanjay Tiwari" href="https://w3.ibm.com/bluepages/profile.html?uid=0591B4744" target="_blank">Sanjay Tiwari(sanjayti@in.ibm.com)</a></td>
+                </tr>
+            </tbody>
           </table>
         </div>
         <p><Link to={"/product"}>PRODUCT PAGE</Link></p>
--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,14 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from "react-router";
-import './product.css'
+import './product.css';
+import { Table } from 'reactstrap';
+import ProductData from '../../data/product.json';
 
-class Product extends React.Component {
+class Product extends Component {
     render() {
+        const data = ProductData.items;
+        console.log("data", data.items);
         return (
             <div className="container">
-                <h1>This is our Product Page</h1>
-                <p>This page is under maintenance.... will be back shortly!</p>
-                <p><Link to={"/"}>HOME</Link></p>
+                <h1>Product Page</h1>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Sku</th>
+                            <th>Brand</th>
+                            <th>Description</th>
+                            <th>Long Description</th>
+                            <th>Price</th>
+                            <th>Size</th>
+                            <th>Color</th>
+                            <th>In Stock</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((d, i) => {
+                            return (
+                                <tr key={i}>
+                                    <td>
+                                        {i+1}
+                                    </td>
+                                    <td>
+                                        {d.sku}
+                                    </td>
+                                    <td>
+                                        {d.brand}
+                                    </td>
+                                    <td>
+                                        {d.description}
+                                    </td>
+                                    <td>
+                                        {d.long_description}
+                                    </td>
+                                    <td>
+                                        {d.price}
+                                    </td>
+                                    <td>
+                                        {d.size}
+                                    </td>
+                                    <td>
+                                        {d.color}
+                                    </td>
+                                    <td>
+                                        {d.in_stock}
+                                    </td>
+                                </tr>
+                            )                             
+                        })}
+                    </tbody>
+                </Table>
+                <Link to={"/"}>Home</Link>
             </div>
         )
     }
